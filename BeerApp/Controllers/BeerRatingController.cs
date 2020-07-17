039b1c55aa97dce94d70faa4959ea767626175c5
Brendan <bbotond@live.com>
2020-07-17 18:04:56 -0400

    checkin

--- BeerApp/Controllers/BeerRatingController.cs
+++ BeerApp/Controllers/BeerRatingController.cs
@@ -118,45 +118,5 @@ namespace BeerApp.Controllers
         }
 
 
-
-        // GET api/<controller>/5
-        /// <summary>
-        /// REST API endpoint to retrieve a list of beers.
-        /// </summary>
-        /// <param name="id">the name of the beer to collect from the punk api</param>
-        /// <returns>josn result of the selected beer and all of its ratings in database.json</returns>
-        public IEnumerable<dynamic> Get(string id)
-        {
-            //send request to punk api for data based off of the beer name
-            string path = "https://api.punkapi.com/v2/beers/?beer_name=" + id;
-            HttpResponseMessage response = client.GetAsync(path).Result;
-
-            if (response.IsSuccessStatusCode)
-            {
-                var Samochod = response.Content.ReadAsStringAsync();
-                List<PunkBeerViewModel> json = JsonConvert.DeserializeObject<List<PunkBeerViewModel>>(Samochod.Result);
-
-                //check that database.json exists and load the data from database.json
-                List<BeerRatingViewModel> punkBeers = new List<BeerRatingViewModel>();
-                if (File.Exists(Models.Db_Json_Path.database_json_Path))
-                {
-                    using (FileStream fs = File.OpenRead(Models.Db_Json_Path.database_json_Path))
-                    {
-                        punkBeers = System.Text.Json.JsonSerializer.Deserialize<List<BeerRatingViewModel>>(fs.ToString());
-                    }
-                }
-                //call to service failed return no data
-                return from c in json
-                       join p in punkBeers on c.id equals p.BeerId into ps
-                       select new { c.id, c.name, c.description, userRatings = ps.Select(ds => new { ds.Rating, ds.Username, ds.Comments }) };
-            }
-            Request.CreateErrorResponse(HttpStatusCode.BadRequest, response.RequestMessage.ToString());
-            return null;
-        }
-
-
-
-
-
     }
 }
